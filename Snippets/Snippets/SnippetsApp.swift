2e4bcdb864d9163626035682203c6dec9076f091
Montana Braswell <montanabraswell1@gmailcom>
2025-09-24 13:42:31 -0400

    add snippet project

---
+++ Snippets/Snippets/SnippetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnippetsApp.swift
+//  Snippets
+//
+//  Created by Montana  on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SnippetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
